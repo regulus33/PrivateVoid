72f44d1840106e5636fb546fc3d03e5248a27519
zachary rowden <zacharyrowden@zacharys-MacBook-Pro.local>
2019-01-09 00:13:37 +0100

    Boiler plate with basic character movement.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // these appear in the gui on under whatever script is applied to from public declaration
+    public Rigidbody2D theRB;
+    public float moveSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Matrix4x4 and y only for 2d
+        theRB.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * moveSpeed; 
+    }
+}
