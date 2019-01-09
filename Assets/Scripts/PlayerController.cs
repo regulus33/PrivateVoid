f867dbb90d2585be01e4652d8d0210838272fa84
zachary rowden <zacharyrowden@zacharys-MacBook-Pro.local>
2019-01-09 05:36:44 +0100

    Finish character player moving anim and scripts.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,7 +7,7 @@ public class PlayerController : MonoBehaviour
     // these appear in the gui on under whatever script is applied to from public declaration
     public Rigidbody2D theRB;
     public float moveSpeed;
-
+    public Animator myAnim;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,5 +19,14 @@ public class PlayerController : MonoBehaviour
     {
         //Matrix4x4 and y only for 2d
         theRB.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * moveSpeed; 
+        myAnim.SetFloat("moveX", theRB.velocity.x);
+        myAnim.SetFloat("moveY", theRB.velocity.y);
+
+        if(Input.GetAxisRaw("Horizontal") == 1 || Input.GetAxisRaw("Horizontal") == -1 || Input.GetAxisRaw("Vertical") == 1 || Input.GetAxisRaw("Vertical") == -1 ) {
+            
+            myAnim.SetFloat("lastMoveX", Input.GetAxisRaw("Horizontal"));
+            myAnim.SetFloat("lastMoveY", Input.GetAxisRaw("Vertical"));
+        
+        }
     }
 }
