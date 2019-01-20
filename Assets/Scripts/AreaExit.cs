06195bcbbcf3e14e97f054ae5663a60495af552e
zachary rowden <zacharyrowden89@gmail.com>
2019-01-20 20:22:48 +0100

    Scene switch fade.

--- Assets/Scripts/AreaExit.cs
+++ Assets/Scripts/AreaExit.cs
@@ -10,6 +10,9 @@ public class AreaExit : MonoBehaviour
     public string areaTransitionName;
     //entry point, set to whatever current value of entrance is here, this logically puts us into the next world. We leave to where we entered from.
     public AreaEntrance theEntrance;
+    private bool shouldLoadAfterFade;
+
+    public float waitToLoad = 1f;
 
     // Start is called before the first frame update
     void Start()
@@ -20,14 +23,26 @@ public class AreaExit : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+         //makes sure area loads after black fade
+        if (shouldLoadAfterFade)
+        {
+            waitToLoad -= Time.deltaTime;
+            if(waitToLoad <= 0)
+            {
+                shouldLoadAfterFade = false;
+                SceneManager.LoadScene(areaToLoad);
+            }
+        }
+
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
        if(other.tag == "Player") 
        {
-           SceneManager.LoadScene(areaToLoad);
+           //SceneManager.LoadScene(areaToLoad);
+            shouldLoadAfterFade = true;
+            UIFade.instance.FadeToBlack();
        }
         //because playercontroller is set to public static there can only ever be one instance, handy for accessing globally
         //this is for entering and exiting scenes.
