ecf0d353cbadd2646ecb0dfc93533fdecb5ac368
zachary rowden <zacharyrowden89@gmail.com>
2019-01-21 23:48:22 +0100

    Basic loop through dialog

---
+++ Assets/Scripts/DialogManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogManager : MonoBehaviour
+{
+    public Text dialogText;
+    public Text nameText;
+    public GameObject dialogBox;
+    public GameObject nameBox;
+
+    public string[] dialogLines;
+
+    public int currentLine;
+
+    void Start()
+    {
+        dialogText.text = dialogLines[currentLine];   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(dialogBox.activeInHierarchy)
+        {
+            //is it being pressed
+            if (Input.GetButtonUp("Fire1"))
+            {
+                currentLine++;
+
+                dialogText.text = dialogLines[currentLine];
+            }
+        }
+
+    }
+}
