07a0112d368aa5cc15cfec0e0e083c1ada5b59a6
zachary rowden <zacharyrowden89@gmail.com>
2019-01-25 22:18:31 +0100

    Disable movement in menu.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,6 +7,8 @@ public class GameManager : MonoBehaviour
     public static GameManager instance;
     public CharStats[] playerStats;
 
+    public bool gameMenuOpen, dialogActive, fadingBetweenAreas;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +20,13 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (gameMenuOpen || dialogActive || fadingBetweenAreas)
+        {
+            PlayerController.instance.canMove = false;
+        }
+        else
+        {
+            PlayerController.instance.canMove = true;
+        }
     }
 }
