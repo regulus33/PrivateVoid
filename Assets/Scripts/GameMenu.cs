3053e1eacaff29468ba6598928ecdb61a1ad5f30
zachary rowden <zacharyrowden89@gmail.com>
2019-01-26 02:17:15 +0100

    Plug player stats game object data into menu displaying scripts.

--- Assets/Scripts/GameMenu.cs
+++ Assets/Scripts/GameMenu.cs
@@ -1,11 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameMenu : MonoBehaviour
 {
     public GameObject theMenu;
-  
+
+    private CharStats[] playerStats;
+    public Text[] nameText, hpText, mpText, levlText, expText;
+    public Slider[] expSlider; 
+    public Image[] charImage;
+    public GameObject[] charStatHolder;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -26,7 +33,38 @@ public class GameMenu : MonoBehaviour
             else
             {
                 theMenu.SetActive(true);
+                UpdateMainStats();
                 GameManager.instance.gameMenuOpen = true;
+            }
+        }
+    }
+
+    public void UpdateMainStats()
+    {
+
+        playerStats = GameManager.instance.playerStats; 
+        for(int i = 0; i < playerStats.Length; i++) 
+        {
+            //if the player object these stats belong to is actively in the game 
+            if(playerStats[i].gameObject.activeInHierarchy)
+            {
+                
+                //get his stat UI container activated
+                charStatHolder[i].SetActive(true);
+                //hook up player stats with UI display
+                nameText[i].text = playerStats[i].charName;
+                hpText[i].text = "HP: " + playerStats[i].currentHP + "/" + playerStats[i].maxHP;
+                mpText[i].text = "MP: " + playerStats[i].currentMP + "/" + playerStats[i].maxMP;
+                levlText[i].text = "MP: " + playerStats[i].playerLevel;
+                expText[i].text = "" + playerStats[i].currentEXP + "/" + playerStats[i].expToNextLevel[playerStats[i].playerLevel];
+                expSlider[i].maxValue = playerStats[i].expToNextLevel[playerStats[i].playerLevel];
+                expSlider[i].value = playerStats[i].currentEXP;
+                charImage[i].sprite = playerStats[i].charImage;
+
+            } else {
+
+                charStatHolder[i].SetActive(false);
+
             }
         }
     }
