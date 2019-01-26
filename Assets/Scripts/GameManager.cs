670dbfe0b0d863edd84378e106890089c182be3d
zachary rowden <zacharyrowden89@gmail.com>
2019-01-26 22:06:31 +0100

    Item grid.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,6 +9,10 @@ public class GameManager : MonoBehaviour
 
     public bool gameMenuOpen, dialogActive, fadingBetweenAreas;
 
+    public string[] itemsHeld; //this and below reference eachother
+    public int[] numberOfItems;
+    public Item[] referenceItems; 
+
     // Start is called before the first frame update
     void Start()
     {
@@ -28,5 +32,16 @@ public class GameManager : MonoBehaviour
         {
             PlayerController.instance.canMove = true;
         }
+    }
+
+    public Item GetItemDetails(string itemToGrab) 
+    {
+        for(int i=0; i < referenceItems.Length; i++)
+        {
+            if(referenceItems[i].itemName == itemToGrab){
+                return referenceItems[i];
+            }
+        }
+        return null; 
     }
 }
