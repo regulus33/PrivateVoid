c192ce3de9cd4a71032776902916946146e790ab
zachary rowden <zacharyrowden89@gmail.com>
2019-02-13 08:08:35 +0100

    Initial quest tracking

---
+++ Assets/Scripts/QuestManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestManager : MonoBehaviour
+{
+    public string[] questMarkerNames;
+    public bool[] questMarkersComplete;
+
+    public static QuestManager instance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;
+        questMarkersComplete = new bool[questMarkerNames.Length];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Q))
+        {
+            Debug.Log(CheckIfComplete("quest test"));
+            MarkQuestComplete("quest test");
+            MarkQuestIncomplete("fight the demon");
+        }
+    }
+
+    public int GetQuestNumber(string questToFind)
+    {
+        for(int i=0; i < questMarkerNames.Length; i++ )
+        {
+            if(questMarkerNames[i] == questToFind)
+            {
+                return i ;
+            }
+        }
+        Debug.LogError("Quest: " + questToFind + " does not exist");
+        return 0;
+    }
+
+    public bool CheckIfComplete(string questToCheck)
+    {   //if zero, there is no quest with this name
+        if(GetQuestNumber(questToCheck) > 0)
+        {
+            return questMarkersComplete[GetQuestNumber(questToCheck)];
+        }
+        return false;
+    }
+
+    public void MarkQuestComplete(string questToMark)
+    {
+        questMarkersComplete[GetQuestNumber(questToMark)] = true;
+    }
+
+    public void MarkQuestIncomplete(string questToMark)
+    {
+        questMarkersComplete[GetQuestNumber(questToMark)] = false; 
+    }
+}
