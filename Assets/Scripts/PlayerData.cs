71181a1aca8ae9f82c9828f7e4ec9cd7bae5205c
zachary rowden <zacharyrowden89@gmail.com>
2019-02-27 04:11:28 +0100

    More tests and setup for playerdata TODO add data on playercontroller to data object.

---
+++ Assets/Scripts/PlayerData.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerData : MonoBehaviour
+{
+    public int hp = 100;
+
+    public int porkChopImpact = 20;
+
+    public int maxHp = 100;
+
+    public string[] items;
+
+    public int withdrawal;
+
+    public int intWithdrawalImpact = 10;
+    public int maxWithdraw = 100; 
+
+    public int exp; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void addHp(int hpToAdd)
+    {   if(hpToAdd + hp <= maxHp)
+        {
+            hp += hpToAdd;
+        }
+    }
+
+    public void deferWithdrawal(int amount)
+    {
+        if(withdrawal - amount > 0)
+        {
+            withdrawal -= amount;
+        }
+    }
+
+    public void UseItem(string itemName)
+    {
+        if(itemName == "PorkChop")
+        {
+            addHp(porkChopImpact);
+        }
+        if(itemName == "PillBottle")
+        {
+            deferWithdrawal(intWithdrawalImpact);
+        }
+
+    }
+
+}
---
+++ Assets/Tests/PlayerDataTests.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class PlayerDataTests
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void Use_Item_Works()
+        {
+            var playerData = new GameObject().AddComponent<PlayerData>();
+            playerData.hp = 0;
+            playerData.UseItem("PorkChop");
+
+            Assert.AreEqual(playerData.hp, playerData.porkChopImpact);
+        }
+
+    }
+}
